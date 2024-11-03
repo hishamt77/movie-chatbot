81149d80a2fec63680a22a07fca79fb8fcfd3b70
hishamt77 <mh852129@gmail.com>
2024-11-03 08:09:07 +0530

    delete

--- models/movie.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const MovieSchema = new mongoose.Schema({
-    title: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Movie', MovieSchema);
